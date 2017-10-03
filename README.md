e886cef409fd4022a3dffcdf28022574d1eb8a99
Bhuvnesh Bansal <bhuvnesh.bansal@197NODMB17334.local>
2017-10-03 18:02:53 +0530

    Added LQIP support

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 # html-inline-image-lqip
 A npm package to convert your local image references in any dom element with inline base 64 data uri. This technique is beneficial for saving extra image requests and rendering lightning fast images.
+
+#Support for LQIP (Low Quality Image Placeholder) @bbansal
+For LQIP we need to add two data-attributes
+- Need to set lqip true 
+- Need to padd lqip-parent-url to high resolution url path
+
+It will add two child divs under main div. You can add styling or animation on high resolution image load on basis of class 'aspectRatioPlaceholder-fill' of child div added.
--- test/test.js
+++ test/test.js
@@ -18,13 +18,34 @@ describe('#html-inline-image-processor', function() {
     console.log(result.toString())
     let src = cheerio.load(result.toString())('div').attr('style');
     expect(src)
-      .to.equal("{background:url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=')}");
+      .to.equal("background:url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=');");
   });
   it('should read file content and convert style background url to data uri', function() {
     var result = inlineImage(path.resolve(__dirname,'divtest.html'),path.resolve(__dirname));
     console.log(result.toString())
     let src = cheerio.load(result.toString())('div').attr('style');
     expect(src)
-      .to.equal("{background:url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=')}");
+      .to.equal("background:url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=');");
   });
+
+  describe('#lqip feature test success cases',() => {
+    it('should append div element as child for high resolution image',() => {
+      var html = fs.readFileSync(path.resolve(__dirname,'divtest-lqip.html'));
+      var result = inlineImage(html,path.resolve(__dirname));
+      console.log(result.toString());
+      let childrenDiv = cheerio.load(result.toString())('div').children('.aspectRatioPlaceholder-fill');
+      expect(childrenDiv.length).to.equal(1);
+    });
+  })
+
+  describe('#lqip feature test negative cases',() => {
+    it('should append div element as child for high resolution image fail',() => {
+      var html = fs.readFileSync(path.resolve(__dirname,'divtest-lqip-failed.html'));
+      var result = inlineImage(html,path.resolve(__dirname));
+      console.log(result.toString());
+      let childrenDiv = cheerio.load(result.toString())('div').children('.aspectRatioPlaceholder-fill');
+      expect(childrenDiv.length).to.equal(0);
+    });
+  })
+  
 });
